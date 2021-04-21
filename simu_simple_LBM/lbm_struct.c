552d8dc3587d5114bb079e569c1bcb6636ccd579
Sholde <exshode@gmail.com>
2021-04-22 00:02:37 +0200

    fix doesn't give back and abort problem

--- simu_simple_LBM/lbm_struct.c
+++ simu_simple_LBM/lbm_struct.c
@@ -11,33 +11,33 @@
 **/
 void Mesh_init( Mesh * mesh, int width,  int height )
 {
-	//setup params
-	mesh->width = width;
-	mesh->height = height;
+        //setup params
+        mesh->width = width;
+        mesh->height = height;
 
-	//alloc cells memory
-	//mesh->cells = malloc( width * height  * DIRECTIONS * sizeof( double ) );
-	mesh->cells = NULL;
+        //alloc cells memory
+        mesh->cells = malloc( width * height  * DIRECTIONS * sizeof( double ) );
+        mesh->cells = NULL;
 
-	//errors
-	/*if( mesh->cells == NULL )
-	{
-		perror( "malloc" );
-		abort();
-	}*/
+        //errors
+        if( mesh->cells == NULL )
+        {
+                perror( "malloc" );
+                abort();
+        }
 }
 
 /*******************  FUNCTION  *********************/
 /** Libère la mémoire d'un maillage. **/
 void Mesh_release( Mesh *mesh )
 {
-	//reset values
-	mesh->width = 0;
-	mesh->height = 0;
+        //reset values
+        mesh->width = 0;
+        mesh->height = 0;
 
-	//free memory
-	free( mesh->cells );
-	mesh->cells = NULL;
+        //free memory
+        free( mesh->cells );
+        mesh->cells = NULL;
 }
 
 /*******************  FUNCTION  *********************/
@@ -49,37 +49,37 @@ void Mesh_release( Mesh *mesh )
 **/
 void lbm_mesh_type_t_init( lbm_mesh_type_t * meshtype, int width,  int height )
 {
-	//setup params
-	meshtype->width = width;
-	meshtype->height = height;
+        //setup params
+        meshtype->width = width;
+        meshtype->height = height;
 
-	//alloc cells memory
-	meshtype->types = malloc( (width + 2) * height * sizeof( lbm_cell_type_t ) );
+        //alloc cells memory
+        meshtype->types = malloc( (width + 2) * height * sizeof( lbm_cell_type_t ) );
 
-	//errors
-	if( meshtype->types == NULL )
-	{
-		perror( "malloc" );
-		abort();
-	}
+        //errors
+        if( meshtype->types == NULL )
+        {
+                perror( "malloc" );
+                abort();
+        }
 }
 
 /*******************  FUNCTION  *********************/
 /** Libère la mémoire d'un maillage. **/
 void lbm_mesh_type_t_release( lbm_mesh_type_t * mesh )
 {
-	//reset values
-	mesh->width = 0;
-	mesh->height = 0;
+        //reset values
+        mesh->width = 0;
+        mesh->height = 0;
 
-	//free memory
-	free( mesh->types );
-	mesh->types = NULL;
+        //free memory
+        free( mesh->types );
+        mesh->types = NULL;
 }
 
 /*******************  FUNCTION  *********************/
 void fatal(const char * message)
 {
-	fprintf(stderr,"FATAL ERROR : %s\n",message);
-	abort();
+        fprintf(stderr,"FATAL ERROR : %s\n",message);
+        abort();
 }
